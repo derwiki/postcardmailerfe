a9389cda93f7c0d1dec717bb693959a4f5750a43
Adam Derewecki <derewecki@gmail.com>
2020-07-24 09:28:38 -0700

    Wheelbarrow Signup,Signin,About components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'reactstrap';
+// import logo from './logo.svg';
 import './App.css';
+import SignupComponent from './components/SignupComponent';
+import SigninComponent from './components/SigninComponent';
+import AboutComponent from './components/AboutComponent';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row>
+          <Col className='col px-0'>
+            <div id='hero' />
+            <h1 className='text-center m-2'>
+              Send a Free Postcard Right Now
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <SignupComponent />
+        <SigninComponent />
+        <AboutComponent />
+      </Container>
     </div>
   );
 }
