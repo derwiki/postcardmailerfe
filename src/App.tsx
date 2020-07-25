ca6a33327eee3fcd7f3f581fa9257153a0b15b3e
Adam Derewecki <derewecki@gmail.com>
2020-07-25 13:57:29 -0700

    update test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-// import logo from './logo.svg';
 import './App.css';
 import SignupComponent from './components/SignupComponent';
 import SigninComponent from './components/SigninComponent';
 import AboutComponent from './components/AboutComponent';
 
+const API_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "access-control-allow-origin, access-control-allow-headers",
+};
+
+const fetchTest = () => {
+  fetch('https://postcardmailerapi.herokuapp.com/v1/signup', {
+    method: 'POST',
+    body: JSON.stringify({
+      'email': 'a@der.wiki',
+      'password': 'hunter2'
+    }),
+    headers: API_HEADERS
+  }).then(response => response.json())
+    .then(respJson => console.log('then', respJson))
+    .catch(resp => console.error('catch', resp));
+}
 
 const App = () => {
+  fetchTest();
+
   return (
     <div className="App">
       <Container fluid>
