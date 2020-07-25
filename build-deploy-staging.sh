079d82f358e247ecf30e463f51247dbd3e47de93
Adam Derewecki <derewecki@gmail.com>
2020-07-25 13:57:50 -0700

    add --acl to deploy script

--- build-deploy-staging.sh
+++ build-deploy-staging.sh
@@ -3,6 +3,6 @@
 time (\
 	cd ~/src/postcardmailerfe && \
 	npm run build && \
-	aws s3 sync --cache-control 'max-age=604800' --exclude index.html build/ s3://staging.postcardmailer.us/ && \
-  aws s3 sync --cache-control 'no-cache' build/ s3://staging.postcardmailer.us/
+	aws s3 sync --acl public-read --cache-control 'max-age=604800' --exclude index.html build/ s3://staging.postcardmailer.us/ && \
+  aws s3 sync --acl public-read --cache-control 'no-cache' build/ s3://staging.postcardmailer.us/
 )
