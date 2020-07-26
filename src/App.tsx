b1b9de4aa270bb158a394936aac6e12fa9feba24
Adam Derewecki <derewecki@gmail.com>
2020-07-26 14:12:44 -0700

    postcardPreviewPost

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,54 @@ import SignupComponent from './components/SignupComponent';
 import SigninComponent from './components/SigninComponent';
 import AboutComponent from './components/AboutComponent';
 
-const API_HEADERS = {
+const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "access-control-allow-origin, access-control-allow-headers",
 };
 
-const fetchTest = () => {
-  fetch('https://postcardmailerapi.herokuapp.com/v1/signup', {
+const postcardPreviewPost = () => {
+  fetch('https://postcardmailerapi.herokuapp.com/v1/postcard/preview', {
     method: 'POST',
-    body: JSON.stringify({
-      'email': 'a@der.wiki',
-      'password': 'hunter2'
-    }),
-    headers: API_HEADERS
+    body: JSON.stringify(
+      {
+        "Front": "<html><body>Front</body></html>",
+        "Back": "<html><body>Back</body></html>",
+        "To": [
+          {
+            "Name": "Adam Derewecki",
+            "AddressLine1": "960 Wisconsin St",
+            "AddressLine2": "",
+            "City": "San Francisco",
+            "State": "CA",
+            "Zip": "94107"
+          },
+          {
+            "Name": "Edie Derewecki",
+            "AddressLine1": "609 Cambridge Road",
+            "AddressLine2": "",
+            "City": "Coshocton",
+            "State": "OH",
+            "Zip": "43812"
+          }
+        ],
+        "From": {
+          "Name": "Adam Derewecki",
+          "AddressLine1": "960 Wisconsin St",
+          "AddressLine2": "",
+          "City": "San Francisco",
+          "State": "CA",
+          "Zip": "94107"
+        }
+      }
+    ),
+    headers
   }).then(response => response.json())
     .then(respJson => console.log('then', respJson))
     .catch(resp => console.error('catch', resp));
 }
 
 const App = () => {
-  fetchTest();
+  postcardPreviewPost();
 
   return (
     <div className="App">
