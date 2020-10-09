0d4593eb311ab8bc69184bae98cf8880bcec9a6f
Adam Derewecki <derewecki@gmail.com>
2020-10-09 07:26:04 -0700

    Build:  REACT_APP_API_HOST='https://api.postcardmailer.org'

--- build-deploy-staging.sh
+++ build-deploy-staging.sh
@@ -2,7 +2,7 @@
 
 time (\
   cd ~/src/postcardmailerfe && \
-  REACT_APP_API_HOST='https://postcardmailerapi.herokuapp.com' npm run build && \
+  REACT_APP_API_HOST='https://api.postcardmailer.org' npm run build && \
   aws s3 sync --acl public-read --cache-control 'max-age=604800' --exclude index.html build/ s3://staging.postcardmailer.us/ && \
   aws s3 sync --acl public-read --cache-control 'no-cache' build/ s3://staging.postcardmailer.us/
 )
