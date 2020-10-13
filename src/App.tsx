ca308230eb19d9f7463968ea30f63ef1907bddf6
Adam Derewecki <derewecki@gmail.com>
2020-10-12 18:36:31 -0700

    Fix Navbar nesting

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,60 @@ import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import SignupComponent from './components/SignupComponent';
 import SigninComponent from './components/SigninComponent';
+import NavbarComponent from './components/NavbarComponent';
 import AboutComponent from './components/AboutComponent';
 import AddressesListComponent from './components/AddressesListComponent';
+import UserContext from './components/UserComponent';
 
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "access-control-allow-origin, access-control-allow-headers",
+const user = {
+  signedIn: false,
+}
+
+const initialState = {
+  isAuthenticated: false,
+};
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isAuthenticated: true,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    default:
+      return state;
+  }
 };
 
-
 const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
   return (
-    <div className="App">
-      <Container fluid>
-        <Row>
-          <Col className='col px-0'>
-            <div id='hero' />
-            <h1 className='text-center m-2'>
-              Send a Free Postcard Right Now
-            </h1>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <SignupComponent />
-        <SigninComponent />
-        <AddressesListComponent />
-        <AboutComponent />
-      </Container>
-    </div>
+    <UserContext.Provider value={{state, dispatch}}>
+      <div className="App">
+        <NavbarComponent toggle={() => true} />
+        <Container fluid>
+          <Row>
+            <Col className='col px-0'>
+              <div id='hero' />
+              <h1 className='text-center m-2'>
+                Send a Free Postcard Right Now
+              </h1>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <SignupComponent />
+          <SigninComponent />
+          <AddressesListComponent />
+          <AboutComponent />
+        </Container>
+      </div>
+    </UserContext.Provider>
   );
 }
 
