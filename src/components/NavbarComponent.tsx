9e9c95075349de996cdb30afaaad0233a58429de
Adam Derewecki <a@der.wiki>
2021-01-02 08:20:08 -0800

    g add src/components/NavbarComponent.tsx

---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from "react";
+import { Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown } from "reactstrap";
+import UserContext from "./UserComponent";
+
+function NavbarComponent(props: any) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const user = useContext(UserContext);
+    const { isAuthenticated } = props;
+    console.log("NavbarComponent user", user)
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">
+                    <img src="/favicon-32x32.png" alt="Postcard clipart" className="pr-3" />
+                    Postcard Mailer
+                </NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        {!isAuthenticated && (
+                            <NavItem>
+                                <NavLink href="#">Sign in</NavLink>
+                            </NavItem>
+                        )}
+                        {!isAuthenticated && (
+                            <NavItem>
+                                <NavLink href="#">Sign out</NavLink>
+                            </NavItem>
+                        )}
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavbarComponent
